d1181ba3068e83a2d3b5fd1adea15310918b29e4
Yadubir <yadubirjha2022@vitbhopal.ac.in>
2024-01-19 01:24:28 +0530

    added links

---
+++ index.js
@@ -0,0 +1,8 @@
+let git = document.querySelector('#git');
+let linkedin = document.querySelector('#linkedin');
+git.addEventListener('click', function(){
+    window.open('https://github.com/Yadubir', '_blank');
+});
+linkedin.addEventListener('click', function(){
+    window.open('https://www.linkedin.com/in/yadubir-jha', '_blank');
+});
